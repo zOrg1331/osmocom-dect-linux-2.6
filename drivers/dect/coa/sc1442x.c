26d0d967632553ebebe2474a7bef420ba423945d
Patrick McHardy <kaber@trash.net>
2009-05-07 02:29:36 +0200

    dect: com-on-air: fix up rename
    The symbolic names should use an upper case 'X'.
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- drivers/dect/coa/sc1442x.c
+++ drivers/dect/coa/sc1442x.c
@@ -48,98 +48,98 @@
  * the remaining banks are used to store packet and slot configuration data.
  */
 
-#define SC1442x_DIPSTOPPED		0x80
-#define SC1442x_RAMBANK0		0x00
-#define SC1442x_RAMBANK1		0x04
-#define SC1442x_RAMBANK2		0x08
-#define SC1442x_RAMBANK3		0x0c
-#define SC1442x_RAMBANK4		0x10
-#define SC1442x_RAMBANK5		0x14
-#define SC1442x_RAMBANK6		0x18
-#define SC1442x_RAMBANK7		0x1c
-#define SC1442x_CODEBANK		0x20
-#define SC1442x_BANKSIZE		0x100
+#define SC1442X_DIPSTOPPED		0x80
+#define SC1442X_RAMBANK0		0x00
+#define SC1442X_RAMBANK1		0x04
+#define SC1442X_RAMBANK2		0x08
+#define SC1442X_RAMBANK3		0x0c
+#define SC1442X_RAMBANK4		0x10
+#define SC1442X_RAMBANK5		0x14
+#define SC1442X_RAMBANK6		0x18
+#define SC1442X_RAMBANK7		0x1c
+#define SC1442X_CODEBANK		0x20
+#define SC1442X_BANKSIZE		0x100
 
 /* Interrupts 0-3 */
-#define SC1442x_IRQ_SLOT_0_5		0x01
-#define SC1442x_IRQ_SLOT_6_11		0x02
-#define SC1442x_IRQ_SLOT_12_17		0x04
-#define SC1442x_IRQ_SLOT_18_23		0x08
-#define SC1442x_IRQ_MASK		0x0f
+#define SC1442X_IRQ_SLOT_0_5		0x01
+#define SC1442X_IRQ_SLOT_6_11		0x02
+#define SC1442X_IRQ_SLOT_12_17		0x04
+#define SC1442X_IRQ_SLOT_18_23		0x08
+#define SC1442X_IRQ_MASK		0x0f
 
 /*
  * Burst Mode Controller control information
  */
 
 /* Maximum number of unmasked errors in S-field bits 8 to 31 */
-#define SC1442x_BC0_S_ERR_SHIFT		4
+#define SC1442X_BC0_S_ERR_SHIFT		4
 /* Invert incoming data (RDI) */
-#define SC1442x_BC0_INV_RDI		0x08
+#define SC1442X_BC0_INV_RDI		0x08
 /* Invert outgoing data (TDO) */
-#define SC1442x_BC0_INV_TDO		0x04
+#define SC1442X_BC0_INV_TDO		0x04
 /* Disable writing B-field on A-field CRC error */
-#define SC1442x_BC0_SENS_A		0x02
+#define SC1442X_BC0_SENS_A		0x02
 /* PP/FP mode */
-#define SC1442x_BC0_PP_MODE		0x01
+#define SC1442X_BC0_PP_MODE		0x01
 
 /* Error test mask for S-field bits 15-8 */
-#define SC1442x_BC1_MASK_MASK		0xff
+#define SC1442X_BC1_MASK_MASK		0xff
 
 /* Sliding error test mask for S-field bits 15-8 */
-#define SC1442x_BC2_SLIDE_MASK		0xff
+#define SC1442X_BC2_SLIDE_MASK		0xff
 
 /* DAC output value when BCM is active (for frequency control?) */
-#define SC1442x_BC3_DAC_MASK		0x1f
+#define SC1442X_BC3_DAC_MASK		0x1f
 
 /* Only perform phase jump for correct A-field CRC + SL_EN_ADJ command */
-#define SC1442x_BC4_ADP			0x10
+#define SC1442X_BC4_ADP			0x10
 /* Window in which S-field is accepted */
-#define SC1442x_BC4_WIN_MASK		0x0f
+#define SC1442X_BC4_WIN_MASK		0x0f
 
 /* Amplitude-trimming of gaussian shape */
-#define SC1442x_BC5_VOL_SHIFT		4
+#define SC1442X_BC5_VOL_SHIFT		4
 /* Disable scrambling */
-#define SC1442x_BC5_SC_OFF		0x08
+#define SC1442X_BC5_SC_OFF		0x08
 /* PD1 synchronization pattern:
  * 0 = S-field received, 1 = preamble + first 2 bits of synchronization word */
-#define SC1442x_BC5_DO_FR		0x04
+#define SC1442X_BC5_DO_FR		0x04
 /* TDO output shape */
-#define SC1442x_BC5_TDO_DIGITAL		0x00
-#define SC1442x_BC5_TDO_GAUSIAN		0x01
-#define SC1442x_BC5_TDO_POWER_DOWN	0x02
-#define SC1442x_BC5_TDO_MID_LEVEL	0x03
+#define SC1442X_BC5_TDO_DIGITAL		0x00
+#define SC1442X_BC5_TDO_GAUSIAN		0x01
+#define SC1442X_BC5_TDO_POWER_DOWN	0x02
+#define SC1442X_BC5_TDO_MID_LEVEL	0x03
 
 /* Low 4 bits of multiframe number */
-#define SC1442x_BC6_MFR_SHIFT		4
-#define SC1442x_BC6_MFR_MASK		0xf0
+#define SC1442X_BC6_MFR_SHIFT		4
+#define SC1442X_BC6_MFR_MASK		0xf0
 /* Frame number */
-#define SC1442x_BC6_FR_MASK		0x0f
+#define SC1442X_BC6_FR_MASK		0x0f
 
 /*
  * Burst Mode Controller status information
  */
 
 /* Peak binary value of ADC (RSSI) */
-#define SC1442x_ST0_ADC_MASK		0x3f
+#define SC1442X_ST0_ADC_MASK		0x3f
 
 /* S-pattern recognized according to BMC configuration */
-#define SC1442x_ST1_IN_SYNC		0x80
+#define SC1442X_ST1_IN_SYNC		0x80
 
 /* A-field R-CRC correct */
-#define SC1442x_ST1_A_CRC		0x40
+#define SC1442X_ST1_A_CRC		0x40
 
 /* Protected Bn-subfield R-CRC correct */
-#define SC1442x_ST1_B_CRC_MASK		0x3c
-#define SC1442x_ST1_B1_CRC		0x20
-#define SC1442x_ST1_B2_CRC		0x10
-#define SC1442x_ST1_B3_CRC		0x08
-#define SC1442x_ST1_B4_CRC		0x04
+#define SC1442X_ST1_B_CRC_MASK		0x3c
+#define SC1442X_ST1_B1_CRC		0x20
+#define SC1442X_ST1_B2_CRC		0x10
+#define SC1442X_ST1_B3_CRC		0x08
+#define SC1442X_ST1_B4_CRC		0x04
 
 /* B-field X-CRC correct */
-#define SC1442x_ST1_X_CRC		0x02
+#define SC1442X_ST1_X_CRC		0x02
 
 /* Z-field equals X-CRC */
-#define SC1442x_ST1_Z_CRC		0x01
+#define SC1442X_ST1_Z_CRC		0x01
 
 /* Phase offset of received S-field: which of the nine internal clock cycles
  * per symbol sampled the incoming data. The frequency deviation can be
@@ -147,33 +147,33 @@
  *
  * K * (T / 9) / 10m = K * 96ns / 10m = K * 9.6ppm
  */
-#define SC1442x_ST2_TAP_SHIFT		4
-#define SC1442x_ST2_TAP_MASK		0xf0
+#define SC1442X_ST2_TAP_SHIFT		4
+#define SC1442X_ST2_TAP_MASK		0xf0
 
 /* Number of unmasked S-field errors according to BMC configuration */
-#define SC1442x_ST2_S_ERR_SHIFT		0
-#define SC1442x_ST2_S_ERR_MASK		0x0f
+#define SC1442X_ST2_S_ERR_SHIFT		0
+#define SC1442X_ST2_S_ERR_MASK		0x0f
 
 /* Phase offset of received S-field. */
-#define SC1442x_ST3_PHASE_MASK		0xff
+#define SC1442X_ST3_PHASE_MASK		0xff
 
 /* DC offset of received data to comparator reference input (DAC) */
-#define SC1442x_ST4_DC_MASK		0x3f
+#define SC1442X_ST4_DC_MASK		0x3f
 
 
 static const u8 banktable[] = {
-	SC1442x_RAMBANK1, 0,
-	SC1442x_RAMBANK1, 0,
-	SC1442x_RAMBANK2, 0,
-	SC1442x_RAMBANK2, 0,
-	SC1442x_RAMBANK3, 0,
-	SC1442x_RAMBANK3, 0,
-	SC1442x_RAMBANK4, 0,
-	SC1442x_RAMBANK4, 0,
-	SC1442x_RAMBANK5, 0,
-	SC1442x_RAMBANK5, 0,
-	SC1442x_RAMBANK6, 0,
-	SC1442x_RAMBANK6, 0,
+	SC1442X_RAMBANK1, 0,
+	SC1442X_RAMBANK1, 0,
+	SC1442X_RAMBANK2, 0,
+	SC1442X_RAMBANK2, 0,
+	SC1442X_RAMBANK3, 0,
+	SC1442X_RAMBANK3, 0,
+	SC1442X_RAMBANK4, 0,
+	SC1442X_RAMBANK4, 0,
+	SC1442X_RAMBANK5, 0,
+	SC1442X_RAMBANK5, 0,
+	SC1442X_RAMBANK6, 0,
+	SC1442X_RAMBANK6, 0,
 };
 
 static const u8 jumptable[] = {
@@ -248,10 +248,10 @@ static void sc1442x_write(const struct coa_device *dev, u16 offset, u8 value)
 static void sc1442x_stop_dip(struct coa_device *dev)
 {
 	/* Prevent the interrupt handler from restarting the DIP */
-	dev->ctrl = SC1442x_DIPSTOPPED;
+	dev->ctrl = SC1442X_DIPSTOPPED;
 
 	/* Stop the DIP and wait for interrupt handler to complete */
-	sc1442x_write(dev, dev->cfg_reg, SC1442x_DIPSTOPPED);
+	sc1442x_write(dev, dev->cfg_reg, SC1442X_DIPSTOPPED);
 	synchronize_irq(dev->irq);
 }
 
@@ -328,9 +328,9 @@ static u16 sc1442x_slot_offset(u8 slot)
 {
 	u16 offset;
 
-	offset = SC1442x_BANKSIZE + slot / 4 * SC1442x_BANKSIZE;
+	offset = SC1442X_BANKSIZE + slot / 4 * SC1442X_BANKSIZE;
 	if (slot & 0x2)
-		offset += SC1442x_BANKSIZE / 2;
+		offset += SC1442X_BANKSIZE / 2;
 	return offset;
 }
 
@@ -355,7 +355,7 @@ static void sc1442x_enable(const struct dect_transceiver *trx)
 	u8 slot;
 
 	/* Restore slot table to a pristine state */
-	sc1442x_switch_to_bank(dev, SC1442x_CODEBANK);
+	sc1442x_switch_to_bank(dev, SC1442X_CODEBANK);
 	for (slot = 0; slot < DECT_FRAME_SIZE; slot += 2)
 		sc1442x_write_cmd(dev, patchtable[slot], WNT, 2);
 
@@ -378,7 +378,7 @@ static void sc1442x_confirm(const struct dect_transceiver *trx)
 	 * 24th slot. This must happen within the time it takes to transmit
 	 * 22 slots after the interrupt to lock to the correct signal.
 	 */
-	sc1442x_switch_to_bank(dev, SC1442x_CODEBANK);
+	sc1442x_switch_to_bank(dev, SC1442X_CODEBANK);
 	sc1442x_write_cmd(dev, SyncLoop, BR, SyncLock);
 }
 
@@ -387,7 +387,7 @@ static void sc1442x_unlock(const struct dect_transceiver *trx)
 	const struct coa_device *dev = dect_transceiver_priv(trx);
 
 	/* Restore jump into Sync loop */
-	sc1442x_switch_to_bank(dev, SC1442x_CODEBANK);
+	sc1442x_switch_to_bank(dev, SC1442X_CODEBANK);
 	sc1442x_write_cmd(dev, SyncLoop, BR, Sync);
 }
 
@@ -401,7 +401,7 @@ static void sc1442x_lock(const struct dect_transceiver *trx, u8 slot)
 	 * event. This will automagically establish the correct slot numbers
 	 * and thereby interrupt timing for all slots.
 	 */
-	sc1442x_switch_to_bank(dev, SC1442x_CODEBANK);
+	sc1442x_switch_to_bank(dev, SC1442X_CODEBANK);
 	sc1442x_write_cmd(dev, SyncLoop, BR, jumptable[slot]);
 }
 
@@ -415,7 +415,7 @@ static void sc1442x_set_mode(const struct dect_transceiver *trx,
 
 	switch (mode) {
 	case DECT_SLOT_IDLE:
-		sc1442x_switch_to_bank(dev, SC1442x_CODEBANK);
+		sc1442x_switch_to_bank(dev, SC1442X_CODEBANK);
 		sc1442x_write_cmd(dev, patchtable[slot], WNT, 2);
 		break;
 	case DECT_SLOT_SCANNING:
@@ -424,7 +424,7 @@ static void sc1442x_set_mode(const struct dect_transceiver *trx,
 		off = sc1442x_slot_offset(slot);
 		dev->radio_ops->rx_init(dev, off);
 
-		sc1442x_switch_to_bank(dev, SC1442x_CODEBANK);
+		sc1442x_switch_to_bank(dev, SC1442X_CODEBANK);
 		sc1442x_write_cmd(dev, patchtable[slot], JMP,
 				  sc1442x_rx_funcs[chd->pkt][chd->b_fmt]);
 		break;
@@ -433,7 +433,7 @@ static void sc1442x_set_mode(const struct dect_transceiver *trx,
 		off = sc1442x_slot_offset(slot);
 		dev->radio_ops->tx_init(dev, off);
 
-		sc1442x_switch_to_bank(dev, SC1442x_CODEBANK);
+		sc1442x_switch_to_bank(dev, SC1442X_CODEBANK);
 		sc1442x_write_cmd(dev, patchtable[slot], JMP,
 				  sc1442x_tx_funcs[chd->pkt][chd->b_fmt]);
 		break;
@@ -507,7 +507,7 @@ static u8 sc1442x_clear_interrupt(const struct coa_device *dev)
 	if (int1 == 0xff)
 		return 0;
 
-	int2 = int1 & SC1442x_IRQ_MASK;
+	int2 = int1 & SC1442X_IRQ_MASK;
 
 	/* Clear interrupt status before checking for any remaining events */
 	if (int2 && dev->type == COA_TYPE_PCI)
@@ -520,11 +520,11 @@ static u8 sc1442x_clear_interrupt(const struct coa_device *dev)
 			break;
 		}
 
-		int1 = sc1442x_read(dev, dev->cfg_reg) & SC1442x_IRQ_MASK;
+		int1 = sc1442x_read(dev, dev->cfg_reg) & SC1442X_IRQ_MASK;
 		int2 |= int1;
 	}
 
-	return int2 & SC1442x_IRQ_MASK;
+	return int2 & SC1442X_IRQ_MASK;
 }
 
 static void sc1442x_process_slot(const struct coa_device *dev,
@@ -544,7 +544,7 @@ static void sc1442x_process_slot(const struct coa_device *dev,
 	off = sc1442x_slot_offset(slot);
 
 	/*
-	 * The SC1442x contains a 6 bit ADC for RSSI measurement, convert to
+	 * The SC1442X contains a 6 bit ADC for RSSI measurement, convert to
 	 * units used by the stack.
 	 */
 	rssi = sc1442x_dread(dev, off + SD_RSSI_OFF) * DECT_RSSI_RANGE / 63;
@@ -609,11 +609,11 @@ static void sc1442x_write_bmc_config(const struct coa_device *dev,
 {
 	u8 cfg;
 
-	cfg  = 2 << SC1442x_BC0_S_ERR_SHIFT;
-	cfg |= SC1442x_BC0_INV_TDO;
-	cfg |= SC1442x_BC0_SENS_A;
+	cfg  = 2 << SC1442X_BC0_S_ERR_SHIFT;
+	cfg |= SC1442X_BC0_INV_TDO;
+	cfg |= SC1442X_BC0_SENS_A;
 	if (pp && !tx)
-		cfg |= SC1442x_BC0_PP_MODE;
+		cfg |= SC1442X_BC0_PP_MODE;
 	sc1442x_dwrite(dev, off + 0, cfg);
 
 	/* S-field error mask */
@@ -624,13 +624,13 @@ static void sc1442x_write_bmc_config(const struct coa_device *dev,
 	/* DAC output */
 	sc1442x_dwrite(dev, off + 3, 0);
 
-	cfg  = SC1442x_BC4_ADP;
+	cfg  = SC1442X_BC4_ADP;
 	cfg |= 0xf;
 	cfg |= 0x80;
 	sc1442x_dwrite(dev, off + 4, cfg);
 
-	cfg  = SC1442x_BC5_DO_FR;
-	cfg |= tx ? SC1442x_BC5_TDO_DIGITAL : SC1442x_BC5_TDO_POWER_DOWN;
+	cfg  = SC1442X_BC5_DO_FR;
+	cfg |= tx ? SC1442X_BC5_TDO_DIGITAL : SC1442X_BC5_TDO_POWER_DOWN;
 	sc1442x_dwrite(dev, off + 5, cfg);
 
 	/* Frame number */
@@ -658,8 +658,8 @@ static int sc1442x_check_dram(const struct coa_device *dev)
 	for (bank = 0; bank < 8; bank++) {
 		sc1442x_switch_to_bank(dev, 4 * bank);
 
-		off = bank * SC1442x_BANKSIZE;
-		for (i = 0; i < SC1442x_BANKSIZE - 2; i++)
+		off = bank * SC1442X_BANKSIZE;
+		for (i = 0; i < SC1442X_BANKSIZE - 2; i++)
 			sc1442x_dwrite(dev, off + i, bank + i);
 	}
 
@@ -667,8 +667,8 @@ static int sc1442x_check_dram(const struct coa_device *dev)
 	for (bank = 0; bank < 8; bank++) {
 		sc1442x_switch_to_bank(dev, 4 * bank);
 
-		off = bank * SC1442x_BANKSIZE;
-		for (i = 0; i < SC1442x_BANKSIZE - 2; i++) {
+		off = bank * SC1442X_BANKSIZE;
+		for (i = 0; i < SC1442X_BANKSIZE - 2; i++) {
 			val = sc1442x_dread(dev, off + i);
 			if (val != ((bank + i) & 0xff)) {
 				dev_err(dev->dev,
@@ -690,19 +690,19 @@ int sc1442x_init_device(struct coa_device *dev)
 {
 	u8 slot;
 
-	dev->ctrl = SC1442x_DIPSTOPPED;
+	dev->ctrl = SC1442X_DIPSTOPPED;
 
 	if (sc1442x_check_dram(dev) < 0)
 		return -EIO;
 
 	dev_info(dev->dev, "Loading firmware ...\n");
-	sc1442x_switch_to_bank(dev, SC1442x_CODEBANK);
+	sc1442x_switch_to_bank(dev, SC1442X_CODEBANK);
 	sc1442x_to_cmem(dev, sc1442x_firmware, sizeof(sc1442x_firmware));
 
 	sc1442x_clear_interrupt(dev);
 
 	/* Init DIP */
-	sc1442x_switch_to_bank(dev, SC1442x_RAMBANK0);
+	sc1442x_switch_to_bank(dev, SC1442X_RAMBANK0);
 	sc1442x_write_bmc_config(dev, DIP_RF_INIT, false, false);
 	for (slot = 0; slot < DECT_FRAME_SIZE; slot += 2)
 		sc1442x_init_slot(dev, slot);

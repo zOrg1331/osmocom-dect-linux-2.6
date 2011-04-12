cbf1cafc33d30e952f04cd64f52e15d9ac74d121
Patrick McHardy <kaber@trash.net>
2011-04-12 02:06:53 +0200

    dect: csf: fix RFPI RPN parsing
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- include/net/dect/identities.h
+++ include/net/dect/identities.h
@@ -85,7 +85,7 @@ extern u64 dect_build_ari(const struct dect_ari *ari);
 
 #define DECT_RFPI_E_FLAG	0x0080000000000000ULL
 #define DECT_RFPI_ARI_SHIFT	9
-#define DECT_RFPI_RPN_SHIFT	32
+#define DECT_RFPI_RPN_SHIFT	16
 
 struct dect_idi;
 extern bool dect_rfpi_cmp(const struct dect_idi *i1, const struct dect_idi *i2);
